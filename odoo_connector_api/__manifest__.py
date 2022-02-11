146a877f16486d5bf09a872015ab3232e6ce4275
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2022-02-11 12:52:54 +0100

    Upgrade and fixes invoice 22.2

--- odoo_connector_api/__manifest__.py
+++ odoo_connector_api/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'Odoo Connector Api',
-    'version': '14.0.22.1',
+    'version': '14.0.22.2',
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
