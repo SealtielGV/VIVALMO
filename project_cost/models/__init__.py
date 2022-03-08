6a6619bb832e34c71ecb6eb5671fdcf52859eb93
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2022-03-08 14:00:23 -0600

    cambios

--- project_cost/models/__init__.py
+++ project_cost/models/__init__.py
@@ -1,5 +1,6 @@
 from . import account_move
 from . import mrp_production
 from . import project_task
+from . import stock_move
 from . import stock_scrap
 from . import stock_valuation_layer
---
+++ project_cost/models/stock_move.py
@@ -0,0 +1,7 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+class VivalmoStockMove(models.Model):
+    _inherit = 'stock.move'
+
+    task_id = fields.Many2one('project.task',string='Tarea', related="production_id.x_studio_pr")
