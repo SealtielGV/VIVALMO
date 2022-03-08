11a4f71db2bb851627900d9c1cdab4242b9e70b5
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2022-03-08 14:13:07 -0600

    cambios

--- project_cost/models/stock_valuation_layer.py
+++ project_cost/models/stock_valuation_layer.py
@@ -4,5 +4,5 @@ from odoo.exceptions import UserError
 class VivalmoStockValuationLayer(models.Model):
     _inherit = 'stock.valuation.layer'
 
-    x_studio_pr_relacionada = fields.Many2one('project.task',string='PR relacionada')
-    production_id = fields.Many2one('mrp.production',string='MO',)
+    x_studio_pr_relacionada = fields.Many2one('project.task',string='PR relacionada',related='stock_move_id.task_id')
+    production_id = fields.Many2one('mrp.production',string='MO',related='stock_move_id.production_id')
