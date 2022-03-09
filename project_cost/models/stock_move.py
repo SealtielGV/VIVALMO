43720f82cfa30e269ee280cc364444b199abc629
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2022-03-09 13:05:24 -0600

    cambio

--- project_cost/models/stock_move.py
+++ project_cost/models/stock_move.py
@@ -5,3 +5,18 @@ class VivalmoStockMove(models.Model):
     _inherit = 'stock.move'
 
     task_id = fields.Many2one('project.task',string='Tarea',)
+    
+    
+    def write(self,vals):
+        res = super(VivalmoStockMove, self).write(vals)
+        if self.production_id and self.stock_valuation_layer_ids.filtered(lambda s: s.production_id == False):
+            for valuation in self.stock_valuation_layer_ids:
+                if self.production_id.x_studio_pr:
+                    valuation.update({
+                        'x_studio_pr_relacionada':self.production_id.x_studio_pr.id,
+                        'production_id': self.production_id.id
+                    })
+                else:
+                    valuation.update({
+                        'production_id': self.production_id.id
+                    })
