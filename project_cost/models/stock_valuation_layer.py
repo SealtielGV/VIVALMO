3a4227cb4e55d1cdc25702d4c22c8c76f1e3643a
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2022-03-09 11:04:01 -0600

    cambio

--- project_cost/models/stock_valuation_layer.py
+++ project_cost/models/stock_valuation_layer.py
@@ -8,12 +8,12 @@ class VivalmoStockValuationLayer(models.Model):
     production_id = fields.Many2one('mrp.production',string='MO',compute='_compute_production_value')
     
     
-    @api.depends('production_id')
+    @api.depends('stock_move_id')
     def _compute_production_value(self):
         for val in self:
             val.x_studio_pr_relacionada = False
             val.production_id = False
             if val.stock_move_id.production_id:
                 val.production_id = val.stock_move_id.production_id.id
-                if val.stock_move_id.production_id.task_id:
+                if val.stock_move_id.production_id.x_studio_pr:
                     val.x_studio_pr_relacionada = val.stock_move_id.production_id.x_studio_pr.id
