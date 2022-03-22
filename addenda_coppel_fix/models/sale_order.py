76ac8db55e2d4a15c56be1a2ececdd45a2c2e2bc
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2022-03-22 12:30:01 -0600

    addenda coppel fix

--- addenda_coppel_fix/models/sale_order.py
+++ addenda_coppel_fix/models/sale_order.py
@@ -5,7 +5,7 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     
     
-    prepact_cant = fields.Float(digits=(32, 2), string="Piezas por caja")
-    pallet_quantity = fields.Float(digits=(32, 2), string="Total piezas")
+    prepact_cant = fields.Float(digits=(32, 2), string="Piezas por caja[prepactCant]")
+    pallet_quantity = fields.Float(digits=(32, 2), string="Total piezas[palletQuantity]")
 
     
