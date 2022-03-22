635e85841a09676c33c4eda16d703f0fae7e560d
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2022-03-22 12:49:39 -0600

    addenda coppel fix

--- addenda_coppel_fix/models/sale_order.py
+++ addenda_coppel_fix/models/sale_order.py
@@ -1,6 +1,16 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order'
+    
+    
+    partner_name = fields.Char(string='Cliente Nombre',related='partner_id.display_name')
+    
+    
+
+
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     
