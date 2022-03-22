8f94690cccc52f8e04beaf022c721dd8469caf15
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2022-03-22 13:20:05 -0600

    cambio

---
+++ addenda_coppel_fix/models/product_template.py
@@ -0,0 +1,12 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+class ProductTemplate(models.Model):
+    _inherit = 'ProductTemplate'
+    
+    
+    x_code = fields.Char(string='Código Coppel',compute="_compute_x_code",inverse="_set_x_code", store=True)
+    x_size = fields.Char(string='Talla Coppel',compute="_compute_x_size",inverse="_set_x_size", store=True)
+    x_model = fields.Char(string='Modelo Coppel',compute="_compute_x_model",inverse="_set_x_model", store=True)
+ 
+    
--- addenda_coppel_fix/models/sale_order.py
+++ addenda_coppel_fix/models/sale_order.py
@@ -9,13 +9,3 @@ class SaleOrderLine(models.Model):
     partner_name = fields.Char(string='Cliente Nombre',related='partner_id.display_name')
     
     
-
-
-class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
-    
-    
-    prepact_cant = fields.Float(digits=(32, 2), string="Piezas por caja[prepactCant]")
-    pallet_quantity = fields.Float(digits=(32, 2), string="Total piezas[palletQuantity]")
-
-    
