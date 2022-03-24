2df8fdfc760a7a49789a7ca3784fcc93ddf69dd0
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2022-03-24 17:34:05 -0600

    cambio

--- addenda_coppel_fix/models/product_product.py
+++ addenda_coppel_fix/models/product_product.py
@@ -1,7 +1,7 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 
-class ProductTemplate(models.Model):
+class ProductProduct(models.Model):
     _inherit = 'product.product'
     
     
@@ -9,4 +9,12 @@ class ProductTemplate(models.Model):
     x_size = fields.Char(string='Talla Coppel',compute="_compute_x_size",inverse="_set_x_size", store=True)
     x_model = fields.Char(string='Modelo Coppel',compute="_compute_x_model",inverse="_set_x_model", store=True)
  
+ 
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+    
+    
+    x_code = fields.Char(string='Código Coppel',compute="_compute_x_code",inverse="_set_x_code", store=True)
+    x_size = fields.Char(string='Talla Coppel',compute="_compute_x_size",inverse="_set_x_size", store=True)
+    x_model = fields.Char(string='Modelo Coppel',compute="_compute_x_model",inverse="_set_x_model", store=True)
     
